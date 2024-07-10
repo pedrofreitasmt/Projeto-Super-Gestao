0fc30f91715ec0157300989542d779e6c9d3de4f
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-07-10 11:45:42 -0400

    Começando a estruturar o site.

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,3 @@ Route::get('/', [PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [ContatoController::class, 'contato']);
-// nome, categoria, assunto, mensagem
-
-Route::get(
-    '/contato/{nome}/{categoria_id}',
-    function (
-        string $nome = 'Desconhecido',
-        int $categoria_id = 1 // 1 - 'Informação'
-    ) {
-        echo "Estamos aqui: $nome - $categoria_id";
-    }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Z a-z]+');
