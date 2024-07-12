7a98fe86ef5147d2ebe80b6c904655b0b29ffdc0
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-07-12 10:53:13 -0400

    Estudando sobre rotas. Mais especificamente sobre nomeação de rotas, prefixos e fallback.

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,30 @@
 <?php
 
+use App\Http\Controllers\ClientesController;
 use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ProdutosController;
 use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\TesteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', [LoginController::class, 'login'])->name('site.login');
 
-Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
 
-Route::get('/contato', [ContatoController::class, 'contato']);
+// Rotas com prefixo app
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', [ClientesController::class, 'clientes'])->name('app.clientes');
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
+    Route::get('/produtos', [ProdutosController::class, 'produtos'])->name('app.produtos');
+});
+
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial';
+});
