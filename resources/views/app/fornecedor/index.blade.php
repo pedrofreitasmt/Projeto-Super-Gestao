79aba35e1b885badde898274aa67df429b4ed8c1
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-07-16 12:51:36 -0400

    Praticando a variável global "$loop"

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,17 +1,32 @@
 <h3>Fornecedores</h3>
 
-@php
-    /*
-    if (isset($var))
-    */
-@endphp
-
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[0]['status'] }}
-    <br>
-    CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Dado não preenchido.' }}
+    @forelse ($fornecedores as $indice => $fornecedor)
+
+        Iteração atual: {{ $loop->iteration }}
+        <br>
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br>
+        Status: {{ $fornecedor['status'] }}
+        <br>
+        CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não preenchido.' }}
+        <br>
+        Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+        <br>
+        @if ($loop->first)
+            Primeira iteração do Loop
+            <br>
+        @endif
+        <br>
+        @if ($loop->last)
+            Última iteração do Loop
+            <br>
+            Total de registros: {{ $loop->count }}
+        @endif
+        <hr>
+    @empty
+        Não existem fornecedores cadastrados!
+    @endforelse
 
 @endisset
 <br>
