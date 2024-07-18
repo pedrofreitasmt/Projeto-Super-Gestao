1d324e67e3b48e041aa7dcd8951729ce69df4072
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-07-18 09:22:17 -0400

    Criada uma nova migration para incrementar novas tabelas em uma outra migration já existente

--- database/migrations/2024_07_18_131425_alter_fornecedores_novas_colunas.php
+++ database/migrations/2024_07_18_131425_alter_fornecedores_novas_colunas.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('fornecedores', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome', 50);
-            $table->timestamps();
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->string('uf', 2);
+            $table->string('email', 150);
         });
     }
 
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('fornecedores');
+        //
     }
 };
