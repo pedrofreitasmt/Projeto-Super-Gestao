74baa090cf80ef34ed2091dd0dd2d2d232a20884
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-07-29 08:41:44 -0400

    Método salvar criado para realizar a validação dos dados do formulário recebidos pelo usuário.

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SiteContato;
 use Illuminate\Http\Request;
-
+use App\Models\MotivoContato;
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
-        var_dump($_POST);
-        return view("site.contato", ['titulo' => 'Contato']);
+        $motivo_contatos = $motivo_contatos = MotivoContato::all();
+
+        return view("site.contato", ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
+    }
+
+    public function salvar(Request $request)
+    {
+        // Realizar a validação dos dados do formulários recebidos no request
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000'
+        ]);
     }
 }
