5d6a54bffd334fc2880d8927acef5ad94a0b5a5e
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-07-31 10:52:56 -0400

    Adicionado as mensagens de erros na view de formulário

--- resources/views/site/layouts/components/form_contato.blade.php
+++ resources/views/site/layouts/components/form_contato.blade.php
@@ -3,10 +3,15 @@
 <form action={{ route('site.contato') }} method="POST">
     @csrf
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    @if ($errors->has('nome'))
+        {{ $errors->first('nome') }}
+    @endif
     <br>
     <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
     <br>
     <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    {{ $errors->has('email') ? $errors->first('email') : '' }}
     <br>
 
     <select name="motivo_contatos_id" class="{{ $classe }}">
@@ -18,6 +23,7 @@
                 {{ $motivo_contato->motivo_contato }}</option>
         @endforeach
     </select>
+    {{ $errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : '' }}
     <br>
     <textarea name="mensagem" class="{{ $classe }}">
         @if (old('mensagem') != '')
@@ -26,6 +32,7 @@
             Preencha aqui sua mensagem
         @endif
     </textarea>
+    {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
