42f00836591e21e10f423c5113bb07cc1275d497
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-07-31 10:51:18 -0400

    Melhorando as requisições do formulário. Dois arrays criados, um para as requições e o segundo para emitir as mensagens em caso de dados incorretos enviados pelo usuário.

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\SiteContato;
 use Illuminate\Http\Request;
 use App\Models\MotivoContato;
+
 class ContatoController extends Controller
 {
     public function contato(Request $request)
@@ -16,13 +17,31 @@ class ContatoController extends Controller
 
     public function salvar(Request $request)
     {
-        // Realizar a validação dos dados do formulários recebidos no request
-        $request->validate([
-            'nome' => 'required|min:3|max:40',
+        // Realizando a validação dos dados do formulários recebidos no request
+        $regras = [
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
-            'email' => 'required',
-            'motivo_contato' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
+        ];
+
+        // Customizando a validação do formulário em caso de erros
+        $feedback = [
+            'nome.min' => 'O campo nome precisa ter pelo menos 3 caracteres.',
+            'nome.max' => 'O campo nome tem um limite de 40 caracteres.',
+            'nome.unique' => 'O nome inserido já existe.',
+
+            'email.email' => 'O email informado não é válido.',
+
+            'mensagem.max' => 'O campo mensagem tem um limite de 2000 caracteres.',
+
+            'required' => 'O campo :attribute precisa ser preenchido'
+        ];
+
+        $request->validate($regras, $feedback);
+
+        SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
