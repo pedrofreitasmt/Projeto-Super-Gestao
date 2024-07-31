928a02709afef13972287dcd6e25a9e0cc1648dd
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-07-31 10:53:57 -0400

    Primeiro middleware criado e incrementado à rota principal

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,15 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\ProdutosController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\TesteController;
+use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Support\Facades\Route;
 
+
 // VIA GET
-Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::middleware(LogAcessoMiddleware::class)
+    ->get('/', [PrincipalController::class, 'principal'])
+    ->name('site.index');
+
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::get('/login', [LoginController::class, 'login'])->name('site.login');
@@ -28,6 +33,6 @@ Route::prefix('/app')->group(function () {
 
 Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
 
-Route::fallback(function() {
-    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial';
+Route::fallback(function () {
+    echo 'A rota acessada não existe. <a href="' . route('site.index') . '">Clique aqui</a> para ir para a página inicial';
 });
