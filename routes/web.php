d85bd422e830e5112425f428bc9b22e63b5a71dc
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-08-14 10:33:05 -0400

    ajustando as rotas

--- routes/web.php
+++ routes/web.php
@@ -8,30 +8,29 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\ProdutosController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\TesteController;
-use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Support\Facades\Route;
 
 
 // VIA GET
-Route::middleware(LogAcessoMiddleware::class)
-    ->get('/', [PrincipalController::class, 'principal'])
-    ->name('site.index');
-
-Route::middleware(LogAcessoMiddleware::class)
-    ->get('/sobre-nos', [SobreNosController::class, 'sobreNos'])
-    ->name('site.sobrenos');
-
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
-Route::get('/login', [LoginController::class, 'login'])->name('site.login');
+Route::get('/login/{erro?}', [LoginController::class, 'index'])->name('site.login');
 
 // VIA POST
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
+Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login');
 
 // Rotas com prefixo app
-Route::prefix('/app')->group(function () {
-    Route::get('/clientes', [ClientesController::class, 'clientes'])->name('app.clientes');
-    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
-    Route::get('/produtos', [ProdutosController::class, 'produtos'])->name('app.produtos');
+Route::middleware(['log.acesso', 'autenticacao'])->prefix('/app')->group(function () {
+    Route::get('/clientes', [ClientesController::class, 'clientes'])
+        ->name('app.clientes');
+
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])
+        ->name('app.fornecedores');
+
+    Route::get('/produtos', [ProdutosController::class, 'produtos'])
+        ->name('app.produtos');
 });
 
 Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
