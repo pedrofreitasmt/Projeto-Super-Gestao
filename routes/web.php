10404756d24d323c589c305226dab7a5aff0f801
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-08-20 11:39:27 -0400

    adicionando novas rotas

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ClientesController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\ProdutosController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\TesteController;
 use Illuminate\Support\Facades\Route;
-
 
 // VIA GET
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
@@ -23,14 +25,11 @@ Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login'
 
 // Rotas com prefixo app
 Route::middleware(['log.acesso', 'autenticacao'])->prefix('/app')->group(function () {
-    Route::get('/clientes', [ClientesController::class, 'clientes'])
-        ->name('app.clientes');
-
-    Route::get('/fornecedores', [FornecedorController::class, 'index'])
-        ->name('app.fornecedores');
-
-    Route::get('/produtos', [ProdutosController::class, 'produtos'])
-        ->name('app.produtos');
+    Route::get('/home', [HomeController::class, 'index'])->name('app.home');
+    Route::get('/sair', [LoginController::class, 'sair'])->name('app.sair');
+    Route::get('/cliente', [ClienteController::class, 'index'])->name('app.cliente');
+    Route::get('/fornecedor', [FornecedorController::class, 'index'])->name('app.fornecedor');
+    Route::get('/produto', [ProdutoController::class, 'index'])->name('app.produto');
 });
 
 Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
