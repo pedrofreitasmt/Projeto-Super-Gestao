aaeefc7a6519219060f962d08e8a1de262857800
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-08-20 11:40:07 -0400

    novas views criadas

---
+++ resources/views/app/layouts/basico.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Super Gestão - @yield('titulo')</title>
+    <link rel="stylesheet" href="{{ asset("css/estilo_basico.css") }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+    <link rel="stylesheet" href={{ asset('css/login_blade.css') }}>
+</head>
+
+<body>
+    @include('app.layouts.topo') {{-- Incluindo um layout padrão no topo para as views --}}
+    @yield('conteudo') {{-- Puxando o conteudo de cada view --}}
+</body>
+
+</html>
