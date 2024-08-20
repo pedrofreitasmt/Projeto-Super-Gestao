9237fe84710a56810f1b3b027e8f9cfb0b04c4b4
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-08-20 11:39:10 -0400

    excluindo os testes do controller fornecedor

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,37 +8,6 @@ class FornecedorController extends Controller
 {
     public function index()
     {
-        $fornecedores = [
-            0 => [
-                'nome' => 'Fornecedor 1',
-                'status' => 'N',
-                'cnpj' => '0',
-                'ddd' => '011',
-                'telefone' => '00000-0000',
-            ],
-            1 => [
-                'nome' => 'Fornecedor 2',
-                'status'=> 'S',
-                'cnpj' => null,
-                'ddd' => '065',
-                'telefone' => '00000-0000',
-            ],
-            2 => [
-                'nome' => 'Fornecedor 3',
-                'status' => 'S',
-                'cnpj' => null,
-                'ddd' => '011',
-                'telefone' => '00000-0000',
-            ],
-            3 => [
-                'nome' => 'Fornecedor 4',
-                'status' => 'N',
-                'cnpj' => '0',
-                'ddd' => '061',
-                'telefone' => '00000-0000',
-            ]
-        ];
-
-        return view("app.fornecedor.index", compact('fornecedores',));
+        return view('app.fornecedor');
     }
 }
